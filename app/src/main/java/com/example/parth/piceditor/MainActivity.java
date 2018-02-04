ad307954da2b189356071a0331cab9dc0892e3fd
parth <parthcrathod@gmail.com>
2018-02-04 18:28:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/parth/piceditor/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.parth.piceditor;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+
+    }
+
+    private void openGallery()
+    {
+        Intent intent = new Intent(getApplicationContext(),editorwindow.class);
+        startActivity(intent);
+    }
+}
